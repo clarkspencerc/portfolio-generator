29c2fd5915f96b0ca2d08d6a13c9d5763b0c200c
clarkspencerc <clarkspencerc@gmail.com>
2022-05-21 10:58:38 -0600

    opt in to additional info

--- app.js
+++ app.js
@@ -38,9 +38,22 @@ const promptUser = () => {
             }
         }, 
         {
+            type: 'confirm', 
+            name: 'confirmAbout', 
+            message: 'Would you like to enter some information about yourself for an "About" Section?', 
+            default: true 
+        },
+        {
             type: 'input', 
             name: 'about', 
-            message: 'provide some information about yourself:'
+            message: 'provide some information about yourself:', 
+            when: ({ confirmAbout}) => {
+                if(confirmAbout) {
+                    return true; 
+                } else {
+                    return false; 
+                }
+            }
         }
         
     ]);
